f076efe546cfd1625a57ae86fbff72a31004076f
unknown <ukaran11@gmail.com>
2022-02-22 04:07:17 +0530

    Utsav | connected /users route to createUser Controller

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const controllers = require('../controllers');
 const router = Router();
 
 router.get('/', (req, res) => res.send('This is root!'));
+router.post('/users', controllers.createUser);
 
 module.exports = router;
