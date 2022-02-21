2fc325817d224848547ec40ac7d6faeac65a8d06
unknown <ukaran11@gmail.com>
2022-02-22 04:13:00 +0530

    Utsav | Added func getUserById

--- controllers/index.js
+++ controllers/index.js
@@ -26,7 +26,25 @@ const getAllUsers = async (req, res) => {
     }
 }
 
-
+const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findOne({
+            where: {id: id},
+            include: [
+                {
+                    model: Item
+                }
+            ]
+        });
+        if (user) {
+            return res.status(200).json({ user });
+        }
+        return res.status(404).send('User with the specified ID does not exists');
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
 
 module.exports = {
     createUser
