2c9167fb9bb305d0e8dae4f900bd111c7a52c9fc
unknown <ukaran11@gmail.com>
2022-02-22 03:03:52 +0530

    Utsav | Route file

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
-const bodyParser = require('body-parser');
+const { Router } = require('express');
+const controllers = require('../controllers');
+const router = Router();
 
-const PORT = process.env.PORT || 3000;
+router.get('/', (req, res) => res.send('This is root!'));
 
-const app = express();
-
-app.use(bodyParser.json());
-
-app.use('/api', routes);
-
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+module.exports = router;
