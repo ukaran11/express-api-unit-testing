445cd3a9d6001d9f211678fcabefa1c4609425a0
unknown <ukaran11@gmail.com>
2022-02-22 21:10:05 +0530

    Utsav | Added test for user creation

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -12,5 +12,20 @@ describe('User API', () => {
         const res = await request(app).get('/api/users/3');
         expect(res.statusCode).toEqual(200);
         expect(res.body).toHaveProperty('user')
-    })
+    });
+
+    it('should create a new user', async () => {
+        const res = await request(app)
+            .post('/api/users')
+            .send({
+                firstName: 'Bob',
+                lastName: 'Doe',
+                email: 'bob@doe.com',
+                password: '12345678'
+            })
+        expect(res.statusCode).toEqual(201)
+        expect(res.body).toHaveProperty('user')
+    });
+
+    
 })
